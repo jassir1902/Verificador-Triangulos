28c6cfee354a449108c89e58db3dfbb1a79e8cf1
jassir <jassir1902@gmail.com>
2024-08-02 13:59:45 -0400

    Verificador de Triangulos de implementado

--- VerificadorTriangulos/Program.cs
+++ VerificadorTriangulos/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a;
+int b;
+int c;
+
+Console.WriteLine("-----Verificador de Triangulos-----");
+Console.WriteLine("Ingrese numeros enteros para cada lado de un triangulo.");
+Console.Write("Ingrese el lado A del triangulo: ");
+a = VerificarNumeroEntero();
+Console.Write("Ingrese el lado B del triangulo: ");
+b = VerificarNumeroEntero();
+Console.Write("Ingrese el lado C del triangulo: ");
+c = VerificarNumeroEntero();
+
+string tipo = VerificarTriangulo(a,b,c);
+Console.WriteLine($"El triangulo es {tipo}");
+
+string VerificarTriangulo(int a, int b, int c)
+{
+    if(a == b && b == c)
+    {
+        return "Equilatero";
+    }
+    else if(a == b || a == c ||b == c)
+    {
+        return "Isoceles";
+    }
+    else
+    {
+        return "Escaleno";
+    }
+}
+
+int VerificarNumeroEntero()
+{
+    int numero = 0;
+    bool valido = false;
+
+    while (!valido)
+    {
+        string entrada = Console.ReadLine();
+        try
+        {
+            numero = Convert.ToInt32(entrada);
+            valido = true;
+            Console.WriteLine("Has ingresado el numero: " + numero);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Por favor ingresa un numero entero valido.");
+            Console.Write("Vuelva a intentarlo: ");
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: El numero ingresado es demasiado grande o demasiado pequeno.");
+            Console.Write("Vuelva a intentarlo: ");
+        }
+    }
+    return numero;
+}
+
+
--- VerificadorTriangulos/obj/Debug/net8.0/VerificadorTriangulos.AssemblyInfo.cs
+++ VerificadorTriangulos/obj/Debug/net8.0/VerificadorTriangulos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VerificadorTriangulos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e53915ef2470a3c79d7953bf9fcac3f0aab36e79")]
 [assembly: System.Reflection.AssemblyProductAttribute("VerificadorTriangulos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VerificadorTriangulos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
